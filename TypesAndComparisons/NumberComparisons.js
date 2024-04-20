cb39f0b024b704c32967ebaa7d3a03a287f58f75
Mishmoready <moses@missionreadyhq.com>
2024-04-21 10:18:32 +1200

    quick training

---
+++ TypesAndComparisons/NumberComparisons.js
@@ -0,0 +1,10 @@
+console.log(1 < 90);
+
+console.log(1 < 235);
+console.log(235 < 1);
+console.log(101 > 90);
+console.log(1 > 1);
+console.log(5 > 4);
+console.log(30 < 40);
+console.log(10 > 9);
+console.log(100 > 200);
---
+++ TypesAndComparisons/Types.js
@@ -0,0 +1,8 @@
+//Strings
+const dessert = "Cookies";
+console.log(dessert); // Cookies
+console.log(typeof dessert); // string
+
+const score = 42;
+console.log(score);
+console.log(typeof score);
