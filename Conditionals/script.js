9c358538511248c86782baa534277012bde8a8d2
Mishmoready <moses@missionreadyhq.com>
2024-04-29 10:36:37 +1200

    more exercise activity

---
+++ Conditionals/script.js
@@ -0,0 +1,43 @@
+//Update the variable subscribed so that the comparison returns true and the text displays in the console.
+let sucscribed = false;
+const subscribed = true;
+if (subscribed === true) {
+  console.log("Thank you for subscribing!");
+}
+
+// Code an if statement that evaluates if score is greater than 50. Inside the code block output a string with more than 10 characters in the console.
+let score = 100;
+if (score > 50) {
+  console.log("I got 10 Characters or something like that");
+}
+
+// In the first condition, check if grade is not equal to "A". In the second condition, check if subject equals "Math".
+let subject = "Math";
+let grade = "A";
+if (grade !== "A") {
+  console.log(`Give ${subject} a chance`);
+}
+if (subject === Math) {
+  console.log("today's a great day solving equations!");
+}
+
+// Code a condition to display a character's special power if the game character is "Wizard"
+let character = "Wizard";
+if (character === "Wizard") {
+  console.log("Special power: potions");
+}
+
+//Create a const variable named getTicket. It's value should check if the directFlight variable has a boolean value of true.
+let directFlight = true;
+const getTicket = directFlight === true;
+if (getTicket === true) {
+  console.log("Direct flight found");
+}
+
+// Code an if statement that sets ifFreezing to true if degrees is less than or equal to 0
+let degrees = 0;
+let isFreezing = false;
+if (isFreezing <= 0) {
+  isFreezing = true;
+}
+console.log("Is freezing: " + isFreezing);
