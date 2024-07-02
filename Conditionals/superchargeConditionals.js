bf337b790b296d957bab87c4b73f3c7e4a2081aa
Mishmoready <moses@missionreadyhq.com>
2024-07-03 11:05:02 +1200

    adding more files into the Conditionals

---
+++ Conditionals/superchargeConditionals.js
@@ -0,0 +1,16 @@
+// Code an if statement with the condition true. Inside the code block code a string that displays in the console.
+if (true) {
+  console.log("This is true");
+}
+
+// Create a const variable named isRainy and assign a boolean value to it. Then, code an if statement that uses isRainy as a condition. Output something in the console if isRainy is true.
+const isRainy = true;
+if (isRainy) {
+  console.log("it is Rainy");
+}
+
+// Create a constant variable called onVacation, and assign it the value true. Set the variable as the if statement's condition.
+const onVacation = true;
+if (onVacation) {
+  console.log("Send out-of-office email");
+}
