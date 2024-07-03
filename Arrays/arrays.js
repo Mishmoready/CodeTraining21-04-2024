78f4857b5ac11d1481d6cb6ec502173d339f173b
Mishmoready <moses@missionreadyhq.com>
2024-07-03 20:40:09 +1200

    practice arrays

---
+++ Arrays/arrays.js
@@ -0,0 +1,16 @@
+// Creating a variable for each of your friends would make your code long and complex.
+const friend1 = "Tom";
+const friend2 = "Mia";
+const friend3 = "Lee";
+
+// Rather than creating a variable for each friend's name, we can group all of the values into an array using [ and ].
+const friends = ["Tom", "Mia", "Lee"];
+console.log(friends);
+
+// The values of an array are called elements. For example, "Tom", "Mia", and "Lee" are elements of this array.
+const friends2 = ["Tom", "Mia", "Lee"];
+console.log(friends2);
+
+// To create an empty array, we code [ and ].
+const friends3 = [];
+console.log(friends3);
--- Loops/loopsPractice.js
+++ Loops/loopsPractice.js
@@ -158,5 +158,158 @@ for (let i = 5; i >= 0; i--) {
 let acceleration = 0;
 while (acceleration < 3) {
   acceleration++;
-  console.log(acceleration);
+  console.log(acceleration); // 1 to 3
+}
+
+// What does this code display in the console?
+let tracker = 1;
+while (tracker <= 2) {
+  tracker++;
+}
+console.log(tracker); //3
+
+//What does this code display in the console?
+let counter = 0;
+while (counter < 3) {
+  console.log(counter);
+  counter++;
+} // 0 to 2
+
+// Display 1 through 4 in the console.
+let counter3 = 0;
+while (counter3 < 4) {
+  counter3++;
+  console.log(counter3);
+}
+
+// What's the name of the counter variable in this code?
+console.log("Fastest runners:");
+for (let index = 1; index < 4; index++) {
+  console.log(index + ".");
+} // index
+
+//Assemble the for loop to print in the console numbers 1 to 4 .
+for (let i = 1; i < 5; i++) {
+  console.log(i);
+}
+
+// What does this code display in the console?
+for (let i = 0; i < 3; i++) {
+  console.log("Polly wants a cracker!");
+}
+
+// Inside the parentheses, increment the counter variable using the ++ operator.
+console.log("*");
+let line = "";
+for (let i = 1; i <= 5; i++) {
+  line += "~";
+  console.log(line);
+}
+
+// Which variable is the counter variable of the for loop?
+let title = "Numbers:";
+let line2 = " ";
+for (let i = 1; i < 6; i++) {
+  line2 += i;
+}
+console.log(line2);
+
+// Does the code inside the loop run?
+for (let i = -10; i < 0; i++) {
+  console.log(i);
+}
+
+// Code the condition where the loop runs until the counter variable reaches 5, but does not print 5 in the console.
+for (let i = 10; i > 5; i--) {
+  console.log(i);
+}
+
+// Create a counter variable named number and assign it -5.
+let number = -5;
+while (number < 5) {
+  console.log(number);
+  number++;
+}
+
+//Leaving the rest of the code the same, change the condition to prevent an infinite loop. The loop should run at least once.
+let number2 = 0;
+while (number2 <= 0) {
+  console.log(number2);
+  number2++;
+}
+
+// Display 10 through 40 (in increments of 10) in the console by changing the condition.
+let counter4 = 10;
+while (counter4 <= 40) {
+  console.log(counter4);
+  counter4 += 10;
+}
+
+// Change the initial value of the counter variable so that only negative numbers are displayed in the loop.
+let counter5 = -3;
+while (counter5 < 0) {
+  console.log(counter5);
+  counter5 += 1;
+}
+
+//Display 3 through 8 in the console by adding console.log(counter) in the correct position.
+let counter6 = 2;
+while (counter6 < 8) {
+  counter6++;
+  console.log(counter6);
+}
+
+// Inside the code block, increase the counter variable by ten to prevent an infinite loop.
+let counter7 = 50;
+while (counter7 < 100) {
+  counter7 += 10;
+  console.log(counter7);
+}
+
+// Code the missing keyword.
+let lyric = "I want to hold your h";
+for (let i = 0; i < 6; i++) {
+  lyric += "a";
+}
+lyric += "nd";
+console.log(lyric);
+
+// Create the counter variable i and initialize its value to 0.
+const notifications = 4;
+for (i = 0; i < notifications; i++) {
+  console.log("You've got mail!");
+}
+
+// Inside the for loop, display something in the console that includes the counter variable i.
+let guest = 3;
+for (let i = 0; i < guest; i++) {
+  console.log(i);
+}
+
+// Code the missing counter variable and condition of the for loop. The loop should iterate 5 times. Do not alter anything else in the code.
+console.log("*");
+let line3 = "";
+for (i = 0; i < 5; i++) {
+  line3 += "~";
+  console.log(line3);
+}
+
+// Make sure the counter variable increments by 1 during each loop.
+console.log("- -");
+console.log(". .");
+let nose = "  >";
+for (let i = 0; i < 3; i++) {
+  nose += "-";
+}
+console.log(nose);
+console.log(" o ");
+
+// Fill in the parentheses to create a for loop that increments a counter variable i from 1 through 4.
+for (i = 1; i < 5; i++) {
+  console.log("Display Element");
+}
+
+//Code the counter variable with an initial value so that the output starts at 10 and ends at 1 .
+for (i = 10; i > 0; i--) {
+  console.log(i);
 }
